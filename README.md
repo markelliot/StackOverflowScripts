da5fca51c769402d76b870b401b5f213844785f3
Mark Elliot <mark.elliot@gmail.com>
2011-06-28 22:15:00 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+StackOverflow Edit Context Tab Remover
+--------------------------------------
+
+Adds a simple button (\t) to stip tabs from the post material for easier editing. The button applies only to the edit context.
---
+++ SOTabRemover.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name           StackOverflow Edit Context Tab Remover
+// @author         Mark Elliot
+// @description    Adds a button in the SO edit context to replace tab literals (\t) with 4 spaces
+// @include        http://stackoverflow.com/posts/*/edit*
+// @match          http://stackoverflow.com/posts/*/edit*
+// ==/UserScript==
+
+// This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
+// To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/.
+
+function with_jquery(f) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.textContent = "(" + f.toString() + ")(jQuery)";
+    document.body.appendChild(script);
+};
+	
+with_jquery(function($){
+
+	$(function(){
+		$('ul#wmd-button-row li:last')
+			.before('<li class="wmd-button" id="fix-tabs" style="left: 400px; font-family: monospace; padding-top: 3px; font-size: 11px; hover:">\\t</li>')
+			.before('<li class="wmd-spacer" style="left: 375px;"></li>');
+
+		$('li#fix-tabs').click(function(){
+			$('textarea#wmd-input.processed').val($('textarea#wmd-input.processed').text().replace(/\t/g, '    '));
+		});
+	});
+	
+});
